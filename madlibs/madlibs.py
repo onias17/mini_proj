428546b7fecd3a69d387a4fd361f5bb125551501
onias17 <oniasnephiisrael@gmail.com>
2021-03-22 17:06:13 -0700

    madlibs

---
+++ madlibs/madlibs.py
@@ -0,0 +1,21 @@
+# string concatenation
+
+youtuber = "Christ"
+
+print("subscribe to " + youtuber)
+print("subscribe to {}".format(youtuber))
+print(f"subscribe to {youtuber}")
+
+proper_noun1 = input("Proper Noun: ")
+noun1 = input("Noun: ")
+noun2 = input("Noun: ")
+noun3 = input("Noun: ")
+verb1 = input("Verb: ")
+noun4 = input("Noun: ")
+
+madlib = f"The God of our fathers raised up {proper_noun1}, whom ye slew and hanged on a {noun1}. Him hath God exalted with his right {noun2} to be a Prince and a {noun3}, for to {verb1} repentance to Israel, and forgiveness of {noun4}."
+
+print(madlib)
+
+# IDEAS
+# children's game to learn parts of speech with basic precepts
